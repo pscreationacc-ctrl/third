f7ee793d983d01c2685de8dbe668134c30a5c3cc
testing-01debug <aggelospapatheodosiou2@gmail.com>
2025-12-08 17:25:07 +0200

    fixed

--- test_model.py
+++ test_model.py
@@ -1,7 +1,8 @@
 from transformers import pipeline
 import sys
 generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
-    
+
+user_input = sys.argv[1] if len(sys.argv) > 1 else ""
 prompt = f"Answer this question clearly: {user_input}"
 result = generator(prompt, max_length=50, do_sample=True)
 
